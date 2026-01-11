ac0ec576beffd5ad5c61223684203eb1c26043f0
Tamar Elmakies <tamar055670@gmail.com>
2026-01-11 20:05:16 +0200

    Fix import path for TrampolineSlice

--- Client-react/InflatableRental/src/Components/app/store.jsx
+++ Client-react/InflatableRental/src/Components/app/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import UserReducer from "../features/user/userSlice"
-import TrampolineReducer from "../features/trampoline/trampolineSlice"
+import TrampolineReducer from "../features/trampoline/TrampolineSlice"
 import rentReducer from "../features/rents/rentSlice"
 export const store = configureStore({
     reducer: {
